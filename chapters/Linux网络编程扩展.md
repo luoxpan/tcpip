0a246285b50897dd1c86730c4c13fa554e1cd5e2
luoxpan <lluopan@outlook.com>
2024-10-03 17:20:12 +0800

    epoll

--- chapters/Linux网络编程扩展.md
+++ chapters/Linux网络编程扩展.md
@@ -55,6 +55,8 @@ int main(void)
     FD_SET(1,&set);//fd1=1,表示监视fd1
     FD_SET(2,&set);//fd2=1,表示监视fd2
     FD_CLR(2,&set);//fd2=0,表示不监视fd2
+
+    FD_ISSET(3,&set);//fd3是否发生事件
 }
 ```
 
@@ -73,6 +75,91 @@ struct timeval
 若`select`的返回值为大于0的整数，说明相应数量的文件描述符发生了变化。
 
 此时，`fd_set`中仍为1的描述符为发生了事件的文件。例如调用`select`后`readset`中为`00101`，说明`fd2,fd4`等待读取（有读取事件）
+
+
+从[select服务器](https://github.com/luoxpan/tcpip/tree/main/src/select)代码中不难看出基于`select`的I/O复用技术存在的问题：
+
+1. 调用`select`函数后针对所有文件描述符的循环语句
+2. 每次调用`select`函数时都需要向该函数传递监视对象信息
+
+循环语句很好理解，但`select`真正的性能瓶颈往往是第二项，从函数调用来看，我们似乎只是将`fd_set`的地址传给`select`，然而，由于操作的对象是文件描述符，而文件描述符是由操作系统管理的，因此每次调用`select`都需要将`fd_set`从用户态复制到内核态，`select`返回时又需要从内核态复制到用户态，这在高并发情况下会造成性能瓶颈。
+
+除此之外，`select`通常有一个文件描述符数量的限制（在大多数系统上是1024）。`select`通过轮询收集文件描述符的事件信息，这种轮询在大量文件描述符处于非活动状态时浪费CPU资源。
+
+`poll`没有文件描述符数量限制，但并没有解决`select`复制和轮询的问题。
+
+`select`的优点：平台兼容性
+
+## epoll
+```C++
+#include <sys/epoll.h>
+```
+**`epoll`的优点**：
+1. 无需编写以监视状态变化为目的的针对所有文件描述符的循环语句
+2. 调用对应于`select`函数的`epoll_wait`函数时无需再次传递监视对象信息
+
+`epoll_create`:创建保存epoll文件描述符的空间
+
+`epoll_ctl`:向空间注册或注销文件描述符
+
+`epoll_wait`:与select函数类似，等待文件描述符发生变化
+
+为了解决`select`重复复制的问题，`epoll`向操作系统请求创建保存文件描述符的空间，通过`epoll_ctl`请求操作系统完成文件描述符的添加和删除，使用`epoll_wait`等待文件描述符的变化。
+
+`select`通过`fd_set`变量查看监视对象的状态变化，而`epoll`通过`epoll_event`结构体将发生事件的文件描述符集中到一起。
+
+```C++
+struct epoll_event
+{
+    __uint32_t events;
+    epoll_data_t data;
+}
+
+typedef union epoll_data
+{
+    void *ptr;
+    int fd;
+    __uint32_t u32;
+    __uint62_t u64;
+}epoll_data_t;
+```
+声明足够大的`epoll_event`数组后，传递给`epoll_wait`函数时，发生变化的文件描述符信息将填入该数组，无需像`select`函数那样对所有文件描述符进行循环。
+
+```C++
+int epoll_create(int size);
+```
+size:epoll实例的大小，较新的linux内核会忽视size传入，自动调整epoll实例大小
+
+成功返回epoll文件描述符，失败返回-1
+
+```C++
+int epoll_ctl(int epfd,int op,int fd,struct epoll_event * event);
+```
+epfd:epoll例程的文件描述符
+
+op  :用于指定监视对象的添加、删除或更改等操作
+
+* `EPOLL_ADD`,`EPOLL_DEL`,`EPOLL_MOD` 
+
+fd  :需要注册的监视对象文件描述符
+
+event:监视对象的事件类型
+```C++
+struct epoll_event event;
+event.events=EPOLLIN;
+event.data.fd=sockfd;
+epoll_ctl(epfd,EPOLL_CTL_ADD,sockfd,&event);
+```
+`event.events`:
+* `EPOLLIN`:需要读取数据的情况
+* `EPOLLOUT`:输出缓冲为空，可以立即发送数据的情况
+* `EPOLLPRI`:收到OOB数据的情况
+* `EPOLLRDHUP`:断开连接或半关闭的情况，在边缘触发模式下很有用
+* `EPOLLERR`:发生错误的情况
+* `EPOLLER`:以边缘触发的方式得到事件通知
+* `EPOLLONESHOT`:发生一次事件后，相应文件描述符不再收到事件通知，需要向`epoll_ctl`函数的第二个参数传递`EPOLL_CTL_MOD`，再次设置事件。
+
+
 
 
 
---
+++ src/epoll/echo_server.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/epoll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+const int BUF_SIZE = 1024;
+const int EPOLL_SIZE = 50;
+void error_handling(const char *message)
+{
+    std::cerr << message << std::endl;
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        error_handling("Need port for ./server");
+    }
+
+    int serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1)
+    {
+        error_handling("socket() error");
+    }
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    if (bind(serv_sock, (struct sockaddr *)(&serv_addr), sizeof(serv_addr)) == -1)
+    {
+        error_handling("bind() error");
+    }
+
+    if (listen(serv_sock, 5) == -1)
+    {
+        error_handling("listen() error");
+    }
+
+    int epfd = epoll_create(EPOLL_SIZE);
+    if (epfd == -1)
+    {
+        error_handling("epoll_create() error");
+    }
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = serv_sock;
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, serv_sock, &event) == -1)
+    {
+        error_handling("epoll_ctl() for serv_sock error");
+    }
+
+    char buf[BUF_SIZE];
+    struct epoll_event *ep_events = new struct epoll_event[EPOLL_SIZE];
+    while (true)
+    {
+        int event_cnt = epoll_wait(epfd, ep_events, EPOLL_SIZE, -1);
+        if (event_cnt == -1)
+        {
+            error_handling("epoll_wait() error");
+        }
+        for (int i = 0; i < event_cnt; i++)
+        {
+            if (ep_events[i].data.fd == serv_sock)
+            {
+                struct sockaddr_in clnt_addr;
+                socklen_t adr_size = sizeof(clnt_addr);
+                int clnt_sock = accept(serv_sock, (struct sockaddr *)(&clnt_addr), &adr_size);
+                if (clnt_sock == -1)
+                {
+                    error_handling("accept() error");
+                }
+                event.events = EPOLLIN;
+                event.data.fd = clnt_sock;
+                if (epoll_ctl(epfd, EPOLL_CTL_ADD, clnt_sock, &event) == -1)
+                {
+                    error_handling("epoll_ctl() for clnt_sock error()");
+                }
+                printf("connected client:%d\n", clnt_sock);
+            }
+            else
+            {
+                int str_len = read(ep_events[i].data.fd, buf, BUF_SIZE);
+                if (str_len == 0)
+                {
+                    if (epoll_ctl(epfd, EPOLL_CTL_DEL, ep_events[i].data.fd, nullptr) == -1)
+                    {
+                        error_handling("epoll_ctl() for i.data.fd error");
+                    }
+                    close(ep_events[i].data.fd);
+                    printf("colsed client:%d\n", ep_events[i].data.fd);
+                }
+                else
+                {
+                    write(ep_events[i].data.fd, buf, str_len);
+                }
+            }
+        }
+    }
+    close(serv_sock);
+    close(epfd);
+    return 0;
+}
